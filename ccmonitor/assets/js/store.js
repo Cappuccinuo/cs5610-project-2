e5e4f3e458d9d6bb12ed0b75b1c780410302ee0e
tongcs <tong.p@husky.neu.edu>
2018-04-17 15:29:24 -0400

    modify chart data

--- ccmonitor/assets/js/store.js
+++ ccmonitor/assets/js/store.js
@@ -129,7 +129,7 @@ function root_reducer(state0, action) {
   console.log("reducer", action);
   // {posts, users, form} is ES6 shorthand for
   // {posts: posts, users: users, form: form}
-  let reducer = combineReducers({users, login, token, signup, prices, alerts, alert_form, messages});
+  let reducer = combineReducers({users, login, token, signup, prices, historical_prices, alerts, alert_form, messages});
   let state1 = reducer(state0, action);
   console.log("state1", state1);
   return deepFreeze(state1);
