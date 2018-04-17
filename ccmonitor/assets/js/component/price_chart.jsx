a5495264273992b50d795c6011643c584527018d
Yuan Gao <gaoyuanbj@yahoo.com>
2018-04-17 13:24:33 -0400

    fix code

--- ccmonitor/assets/js/component/price_chart.jsx
+++ ccmonitor/assets/js/component/price_chart.jsx
@@ -57,92 +57,92 @@ class PriceChartComponent extends React.Component {
 function getOption(data){
   var option = {
     title: {
-	x: 'center',
-	align: 'right'
+    	x: 'center',
+    	align: 'right'
     },
     toolbox: {
-	feature: {
-	    dataZoom: {
-	        yAxisIndex: 'none'
-	    },
-	    magicType: {type: ['line', 'bar']},
-	    dataView: {readOnly: false},
-	    restore: {},
-	    saveAsImage: {}
-	},
+    	feature: {
+    	    dataZoom: {
+    	        yAxisIndex: 'none'
+    	    },
+    	    magicType: {type: ['line', 'bar']},
+    	    dataView: {readOnly: false},
+    	    restore: {},
+    	    saveAsImage: {}
+    	},
     },
     tooltip: {
-	trigger: 'axis',
-	axisPointer: {
-	    type: 'cross',
-	    animation: false,
-	    label: {
-	        backgroundColor: '#505765'
-	    }
-	}
+    	trigger: 'axis',
+    	axisPointer: {
+    	    type: 'cross',
+    	    animation: false,
+    	    label: {
+    	        backgroundColor: '#505765'
+    	    }
+    	}
     },
     legend: {
-	data:['BTC','ETH','LTC'],
-        x: 'left',
-        textStyle:{color:"white"},
+    	data:['BTC','ETH','LTC'],
+      x: 'left',
+      textStyle:{color:"white"},
     },
     grid: {
-	left: '3%',
-	right: '4%',
-	bottom: 35,
-	containLabel: true
+    	left: '3%',
+    	right: '4%',
+    	bottom: 35,
+    	containLabel: true
     },
     dataZoom: [
-	{
-	    show: true,
-	    realtime: true,
-	    start: 50,
-	    end: 100
-	},
-	{
-	    type: 'inside',
-	    realtime: true,
-	    start: 65,
-	    end: 85
-	}
+  	{
+  	    show: true,
+  	    realtime: true,
+  	    start: 50,
+  	    end: 100
+  	},
+  	{
+  	    type: 'inside',
+  	    realtime: true,
+  	    start: 65,
+  	    end: 85
+  	}
     ],
     xAxis: {
-	type: 'category',
-	boundaryGap: false,
-	data: data.time,
-	axisLine:{
-	  lineStyle:{
-            color:'white',
-	    width:1
-	  }
-        },
+    	type: 'category',
+    	boundaryGap: false,
+    	data: data.time,
+    	axisLine:{
+    	  lineStyle:{
+                color:'white',
+    	    width:1
+    	  }
+      },
     },
     yAxis: {
-	type: 'value',
-        name: 'price ($)',
-	axisLine:{
-	  lineStyle:{
-            color:'white',
-	    width:1
-	  }
-        },
+    	type: 'value',
+      name: 'price ($)',
+    	axisLine:{
+    	  lineStyle:{
+          color:'white',
+    	    width:1
+    	  }
+      },
     },
     series: [
-	{
+	   {
 	    name:'BTC',
 	    type:'line',
 	    data: data.price_btc
-	},
-	{
+	   },
+	   {
 	    name:'ETH',
 	    type:'line',
 	    data: data.price_eth
-	},
-	{
+	   },
+	   {
 	    name:'LTC',
 	    type:'line',
 	    data: data.price_ltc
-	}
+	   }
     ]
   };
   return option;
