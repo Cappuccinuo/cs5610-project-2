58f6134a195ad89c78566a5785e5db7f5ad7d635
Yuqing Cheng <cheng.yuq@husky.neu.edu>
2018-04-18 22:01:17 -0400

    edit deploy config

--- ccmonitor/config/prod.secret.exs
+++ ccmonitor/config/prod.secret.exs
@@ -9,12 +9,13 @@ use Mix.Config
 # kept out of version control and might be hard to recover
 # or recreate for your teammates (or yourself later on).
 config :ccmonitor, CcmonitorWeb.Endpoint,
+  url: [host: "localhost", port: 5108],
   secret_key_base: "T9sn5wFh4N2o4k7KEISnYceH8SSatXHyig7JZyqwsfXBOdFq+YjJo5SwyIjnOS3t"
 
 # Configure your database
 config :ccmonitor, Ccmonitor.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "currency",
-  password: "123456",
-  database: "ccmonitor_prod",
+  username: "postgres",
+  password: "postgres",
+  database: "ccmonitor_dev",
   pool_size: 15
